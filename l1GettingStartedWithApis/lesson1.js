8147d706b987f7f9e4df1fc52d73d2d0a7912570
rose parker <parker.rose@intracitygeeks.org>
2017-08-03 01:21:49 +0000

    Hello Map Quiz

---
+++ l1GettingStartedWithApis/lesson1.js
@@ -0,0 +1,8 @@
+// This API key can be used in this project and with any API that supports it. 
+// To use this key in your application, pass it with the key=API_KEY parameter.
+
+// //different map types
+// Road map is the default
+// Satellite for real images of the world
+// Hyrbrid where there are features like roads overlaid on top of satellite imagery
+// Terrain which show shading for terrain
